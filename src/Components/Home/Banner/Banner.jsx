4266be415e5c5e5c0485bb05ba5e9e81050e27ca
RafatSh-hash <rafatsh800@gmail.com>
2023-09-07 21:38:02 +0600

    Fixed bg white to gray

--- src/Components/Home/Banner/Banner.jsx
+++ src/Components/Home/Banner/Banner.jsx
@@ -4,9 +4,11 @@ import lottieBanner from "../../../assets/LottieFiles/bannerSide.json"
 
 const Banner = () => {
     return (
-        <div id='homeBannerDiv' className='p-10 flex w-full flex-row justify-evenly items-center'>
+
+
+        <div id='homeBannerDiv' className='p-10 flex bg-black bg-opacity-80 w-full flex-row justify-evenly items-center'>
             <div className='w-full md:w-[60%] lg:w-[50%] mx-auto p-2 md:p-5 lg:p-10'>
-                <button className='bg-transparent py-5 text-md md:text-lg lg:text-xl w-full md:w-[70%] lg:w-[75%] font-semibold text-white hover:bg-gray-100 hover:text-black transition-all duration-700 px-10 rounded-full border-2 border-white'>
+                <button className='bg-transparent py-5 text-md md:text-lg lg:text-xl w-full md:w-[70%] lg:w-[75%] font-semibold text-white hover:bg-gray-200 hover:text-black transition-all duration-700 px-10 rounded-full border-2 border-white'>
                     <a href="">Watch the Aurapreneur Story</a>
                 </button>
                 <h1 className='my-5 text-white lg:text-7xl md:text-4xl text-2xl font-bold text-center md:text-left lg:text-left' >
@@ -19,7 +21,7 @@ const Banner = () => {
                 </p>
                 <div className="form-control">
                     <div className="input-group">
-                        <input type="text" placeholder="Your Email Address" className="input w-full md:w-[60%] lg:w-[70%] mx-auto bg-white input-bordered" />
+                        <input type="text" placeholder="Your Email Address" className="input w-full md:w-[60%] lg:w-[70%] mx-auto bg-gray-200 text-black input-bordered" />
                         <button className="btn w-28 md:w-36 lg:w-44">
                             Start Free Trail
                         </button>
@@ -32,6 +34,9 @@ const Banner = () => {
                 </div>
             </div>
         </div>
+
+
+
     );
 };
 
